b1b7782e6adac64f1b03272bcadfc3835bc8be33
severus889700 <severus889700@gmail.com>
2018-10-14 23:59:38 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require 'pry'
+
 
 def fizzbuzz(number)
-  binding.pry
  if number % 3 == 0
     puts "Fizz"
   elsif number % 5 == 0 
